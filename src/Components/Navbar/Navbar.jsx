8937a81ef528959840eb581a65334a4159ed51b3
shreeshasr <shreesha.sr92@gmail.com>
2021-12-02 23:05:42 +0530

    CORRECTED: search component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,14 +9,8 @@ function Navbar() {
                 <a href="https://groww.in"><img src={image} alt="groww"></img></a>
             </div>
             <div className="searchBox">
-                <span>
-                    <input type="text" className="searchInput"></input>
-                </span>
-                <span>
                     <span className="material-icons">search</span>
-                    <span className="placeholderText">"What are you looking for today?"</span>
-                </span>
-                
+                    <input type="text" className="searchInput" placeholder="What are you looking for today?"></input>        
             </div>
             <div className="loginOrRegister">
                 <button>Login/Register</button>
